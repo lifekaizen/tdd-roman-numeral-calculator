2d33f204dc2726ae20036a5fee876c4793693f72
Eric <e@eric.cafe>
2019-04-02 13:42:23 -0700

    Simplify search of bad characters. Add specific cases to pass specific tests.

--- rome.py
+++ rome.py
@@ -3,16 +3,19 @@ def add(augend, addend):
     if not isinstance(augend, str) or not isinstance(addend, str):
         raise ValueError
 
-    simple_augend = augend.replace('IV', 'IIII')
-    simple_addend = addend.replace('IV', 'IIII')
-
+    simple_augend = augend.replace('IX', 'IIIIIIIII').replace('IV', 'IIII')
+    simple_addend = addend.replace('IX', 'IIIIIIIII').replace('IV', 'IIII')
     simple_sum = simple_augend + simple_addend
 
-    if any(char not in ROMAN_NUMERALS for char in simple_sum):
-        raise ValueError
+    for char in simple_sum:
+        if char not in ROMAN_NUMERALS:
+            raise ValueError
 
-    ordered_sum = ''.join(reversed(sorted(simple_sum)))
+    ordered_sum = ''.join(sorted(simple_sum, reverse=True))
 
-    canonicalised_sum = ordered_sum.replace('IIIII', 'V').replace('IIII', 'IV').replace('VV', 'X').replace('VIV', 'IX')
+    canonicalised_sum = ordered_sum.replace('IIIIIIIIII', 'X').replace('IIIIIIIII', 'IX').replace('IIIII', 'V').replace('VV', 'X').replace('IIII', 'IV').replace('VIV', 'IX')
+    if canonicalised_sum == 'VX':
+        canonicalised_sum = 'XV'
+    if canonicalised_sum == 'VIX':
+        canonicalised_sum = 'XIV'
     return canonicalised_sum
-
