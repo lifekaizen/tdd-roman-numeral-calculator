05e4c307d01c6751af43a246363bc514dec5f664
Eric <e@eric.cafe>
2019-04-02 13:40:18 -0700

    Change tuples to list for more confident iteration. (I'm not as familiar with tuples.)

--- rome.py
+++ rome.py
@@ -1,4 +1,4 @@
-ROMAN_NUMERALS = ('I', 'V', 'X')
+ROMAN_NUMERALS = ['I', 'V', 'X']
 def add(augend, addend):
     if not isinstance(augend, basestring) or not isinstance(addend, basestring):
         raise ValueError
